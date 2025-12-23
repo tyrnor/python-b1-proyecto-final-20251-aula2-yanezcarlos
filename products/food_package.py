b71b039b473e62d6f9df58627d9f532137bcc829
Carlos Yáñez <carlosyanez997@gmail.com>
2025-12-23 22:15:46 +0100

    Update food_package.py

--- products/food_package.py
+++ products/food_package.py
@@ -1,27 +1,46 @@
 #Write your code here
+from abc import ABC, abstractmethod
 
-class FoodPackage (ABC): 
+class FoodPackage(ABC): 
     @abstractmethod
     def pack(self)  -> str:
         pass
+    
     @abstractmethod
     def material(self) -> str:
         pass
+    
     def describe(self):
         return f"Empaque: {self.pack()} , Material: {self.material()}"    
     
 class Wrapping(FoodPackage):  
   #Write your code here
-  pass
+    def pack(self) -> str:
+       return 'Food Wrap Paper'
+    
+    def material(self) -> str:
+       return 'Aluminium'
 
 class Bottle(FoodPackage):  
   #Write your code here
-  pass
+    def pack(self) -> str:
+       return 'Bottle'
+    
+    def material(self) -> str:
+       return 'Plastic'
       
 class Glass(FoodPackage):  
   #Write your code here
-  pass
+    def pack(self) -> str:
+        return 'Glass'
+    
+    def material(self) -> str:
+        return 'Cardboard'
 
 class Box(FoodPackage):  
   #Write your code here
-  pass
+    def pack(self) -> str:
+        return 'Box'
+    
+    def material(self) -> str:
+        return 'Cardboard'
