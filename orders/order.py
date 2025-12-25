cbbe7cc50e19623ac7396c8d9f5b49dcf950ae4d
Carlos Yáñez <carlosyanez997@gmail.com>
2025-12-26 00:49:11 +0100

    Update order.py

--- orders/order.py
+++ orders/order.py
@@ -1,23 +1,29 @@
-from users import *
-from products import *
+from users import Cashier, Customer
+from products.product import Product
+
 
 class Order:
-  def __init__(self, cashier:Cashier, customer:Customer):
-    self.cashier = cashier
-    self.customer = customer
-    self.products = []
+    def __init__(self, cashier: Cashier, customer: Customer):
+        self.cashier = cashier
+        self.customer = customer
+        self.products = []
 
-  def add(self, product : Product):
-    #Write your code here
-    pass
+    def add(self, product: Product):
+        # Write your code here
+        self.products.append(product)
 
-  def calculateTotal(self) -> float:
-    #Write your code here
-    pass
-  
-  def show(self):    
-    print("Hello : "+self.customer.describe())
-    print("Was attended by : "+self.cashier.describe())
-    for product in self.products:
-      print(product.describe())
-    print(f"Total price : {self.calculateTotal()}")
+    def calculateTotal(self) -> float:
+        # Write your code here
+        total = 0.0
+
+        for product in self.products:
+            total += product.price
+        
+        return total
+
+    def show(self):
+        print("Hello : " + self.customer.describe())
+        print("Was attended by : " + self.cashier.describe())
+        for product in self.products:
+            print(product.describe())
+        print(f"Total price : {self.calculateTotal()}")
