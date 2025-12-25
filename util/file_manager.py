4f24510ca2b9d5a0522c37e87730b0491507edb7
Carlos Yáñez <carlosyanez997@gmail.com>
2025-12-25 17:00:48 +0100

    Update file_manager.py

--- util/file_manager.py
+++ util/file_manager.py
@@ -1,8 +1,27 @@
 import pandas as pd
+
+
 class CSVFileManager:
-  def __init__(self,path: str):
-    self.path = path
-  def read(self) -> str:
-    return pd.read_csv(self.path)  
-  def write(self,dataFrame):
-    pass
+    """
+    Clase encargada de la lectura y escritura de archivos CSV.
+    Se función es tanto cargar los datos desde un archivo CSV y
+    devolverlos en forma de DataFrame como convertir un
+    DataFrame en un archivo CSV y guardarlo.
+    """
+
+    def __init__(self, path: str):
+
+        # Ruta del archiv CSV
+        self.path = path
+
+    def read(self) -> pd.DataFrame:
+        """
+        Lee el archivo CSV de la ruta y devuelve su
+        contenido como un DataFrame de pandas.
+        """
+        return pd.read_csv(self.path)
+
+    def write(self, dataFrame: pd.DataFrame):
+
+        # Convierte un DataFrame en un archivo CSV.
+        dataFrame.to_csv(self.path, index=False)
